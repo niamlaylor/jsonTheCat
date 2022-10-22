414b4a9a344bc4c6b26c19a29084e695adec8588
Liam Naylor <liam@liamnaylor.com>
2022-10-21 18:25:30 -0700

    Add test directory and added comments for easier reading

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,14 @@ const request = require('request');
 
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
+    if (error) { 
+      callback(error, null); // This is an unsuccessful request
     } else {
       const data = JSON.parse(body);
       if (data.length !== 0) {
-        callback(null, data[0].description);
+        callback(null, data[0].description); // This is a successful request where the breed is found
       } else {
-        callback(null, 'Not able to find the breed');
+        callback(null, 'Not able to find the breed'); // This is a successful request but breed not found
       }
     }
   });
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { fetchBreedDescription } = require('./breedFetcher');
 const breedName = process.argv.slice(2);
 
 fetchBreedDescription(breedName, (error, desc) => {
-  if (error) {
+  if (error) { // If an error found when the callback runs, log the error details
     console.log('Error fetch details:', error);
-  } else {
+  } else { // If no error found when the callback runs, log the description returned by the callback
     console.log(desc);
   }
 });
