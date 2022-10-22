8907885792914b97ea59ffe106fda3cc5b94f09d
Liam Naylor <liam@liamnaylor.com>
2022-10-21 18:01:43 -0700

    Refactor to use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.co/v1/breeds/search?q=${args}`, (error, response, body) => {
-  if (error) {
-    console.log('There was an error: ', error.code);
-  } else {
-    const data = JSON.parse(body);
-    if (!data.length) {
-      console.log('Breed not found!');
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length !== 0) {
+        callback(null, data[0].description);
+      } else {
+        callback(null, 'Not able to find the breed');
+      }
     }
-  }
-});
+  })
+};
+
+module.exports = { fetchBreedDescription };
+
