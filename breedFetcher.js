6acf5458f7e17bc3f6e73558272e4dcf7c87f25a
Liam Naylor <liam@liamnaylor.com>
2022-10-21 17:28:06 -0700

    Refactor to fix bug

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,15 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+request(`https://api.thecatapi.co/v1/breeds/search?q=${args}`, (error, response, body) => {
   if (error) {
-    console.log('There was an error: ', error);
-  }
-  const data = JSON.parse(body);
-  if (!data.length) {
-    console.log('Breed not found!');
+    console.log('There was an error: ', error.code);
   } else {
-    console.log(data[0].description);
-  }
+    const data = JSON.parse(body);
+    if (!data.length) {
+      console.log('Breed not found!');
+    } else {
+      console.log(data[0].description);
+    };
+  };
 });
